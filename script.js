9da1b292b7cc8462d56969e944c38518a5f7e5b0
deepu8765 <107336249+deepu8765@users.noreply.github.com>
2024-09-07 15:00:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your code here
+
+//         <label for="tos">I agree to the TOS</label>
+//         <input type="checkbox" id="tos" name="tos" required>
+
+//         <button type="submit">Submit</button>
+//     </form>
